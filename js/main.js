7d90109289034afe5677ac85d28287c414b8165d
Jonny Warren <jonnywarren06@gmail.com>
2016-02-22 16:25:41 -0500

    able to generate array of random assets

--- js/main.js
+++ js/main.js
@@ -2,4 +2,29 @@ import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
 
-console.log('Hello, World');
+function Asset(id, location, make) {
+  this.id = id;
+  this.location = location;
+  this.make = make;
+}
+
+var intervals = [];
+
+for (var i = 0; i < 100; i++) {
+
+  var id = i;
+
+  var locations = ["Atlanta", "Decatur", "Macon", "Chicago", "New York", "Seatle"];
+
+  var location = locations[Math.floor(Math.random() * locations.length)];
+
+  var makes = ["Honda", "Chevy", "Ford", "BMW", "Mercedes", "Porsche"];
+
+  var make = makes[Math.floor(Math.random() * makes.length)];
+
+  var asset = new Asset(id, location, make);
+  intervals.push(asset);
+}
+
+console.log(intervals);
+
