7f2f4eefb8c1338919912513ae98754715b7d515
Jonny Warren <jonnywarren06@gmail.com>
2016-02-22 18:25:52 -0500

    working on generating intervals

--- app/js/main.js
+++ app/js/main.js
@@ -19,24 +19,60 @@ function Asset(id, location, make) {
   this.id = id;
   this.location = location;
   this.make = make;
+  this.time = time + ' hrs';
 }
+
+function Interval(start, finish, randomAsset) {
+  this.start = start;
+  this.finish = finish;
+  this.randomAsset = randomAsset;
+}
+
+var assets = [];
 
 var intervals = [];
 
-for (var i = 0; i < 100; i++) {
-
+for (var i = 0; i < 20; i++) {
   var id = i;
-
   var locations = ["Atlanta", "Decatur", "Macon", "Chicago", "New York", "Seatle"];
-
   var location = locations[Math.floor(Math.random() * locations.length)];
-
   var makes = ["Honda", "Chevy", "Ford", "BMW", "Mercedes", "Porsche"];
-
   var make = makes[Math.floor(Math.random() * makes.length)];
-
+  var time = Math.floor(Math.random() * 5 + 1);
   var asset = new Asset(id, location, make);
-  intervals.push(asset);
+  assets.push(asset);
+}
+
+for (var i = 0; i < 1; i++) {
+  var intervalOne = new Interval(start, finish, randomAsset);
+  var start = "12:00am";
+  var finish = "5:59am";
+  var randomAsset = assets[Math.floor(Math.random() * assets.length)];
+  intervals.push(intervalOne);
+}
+
+for (var i = 0; i < 1; i++) {
+  var intervalTwo = new Interval(start, finish, randomAsset);
+  var start = "6:00am";
+  var finish = "11:59pm";
+  var randomAsset = assets[Math.floor(Math.random() * assets.length)];
+  intervals.push(intervalTwo);
+}
+
+for (var i = 0; i < 1; i++) {
+  var intervalThree = new Interval(start, finish, randomAsset);
+  var start = "12:00pm";
+  var finish = "5:59pm";
+  var randomAsset = assets[Math.floor(Math.random() * assets.length)];
+  intervals.push(intervalThree);
+}
+
+for (var i = 0; i < 1; i++) {
+  var intervalFour = new Interval(start, finish, randomAsset);
+  var start = "6:00pm";
+  var finish = "11:59pm";
+  var randomAsset = assets[Math.floor(Math.random() * assets.length)];
+  intervals.push(intervalFour);
 }
 
 console.log(intervals);
